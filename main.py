6fef0d83d9266a332b6d2b209b5f233309c8f3e4
tsukuyomi-senpai <vlad.gotdeev2002@gmail.com>
2024-10-23 22:14:44 +0200

    added minor changes

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from os import getenv
 import requests
+import json
 load_dotenv()
 
 
 app = Flask(__name__)
 
+def get_data():
+    with open("test_data.json", "r") as f:
+        return json.load(f)
 
 @app.route('/api/login',methods = ['POST'])
 def login():
@@ -17,7 +21,7 @@ def login():
         token = request.headers.get('Authorization')
         if token == getenv("API_TOKEN"):
             data = request.get_json()
-            for i in json:
+            for i in get_data():
                 if data["username"] in i and data["password"] == i[data["username"]]:
                     return requests.get("https://catfact.ninja/fact").json()["fact"]
                 else:
