b724a27ae3e9301356af674c05342a6be2dc8a5e
tsukuyomi-senpai <vlad.gotdeev2002@gmail.com>
2024-10-24 15:32:36 +0200

    added writing logs to file

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ app = Flask(__name__)
 def get_data():
     with open("test_data.json", "r") as f:
         return json.load(f)
+    
+def write_successful_log():
+    with open("log.txt", "a") as f:
+        return f.write("successful" +"\n")
+    
+def write_unsuccessful_log():
+    with open("log.txt", "a") as f:
+        return f.write("Wrong \n")    
 
 @app.route('/api/login',methods = ['POST'])
 def login():
@@ -20,8 +28,10 @@ def login():
             data = request.get_json()
             for i in get_data():
                 if data["username"] in i and data["password"] == i[data["username"]]:
+                    write_successful_log()
                     return requests.get("https://catfact.ninja/fact").json()["fact"]
                 else:
+                    write_unsuccessful_log()
                     return jsonify({"message": "Unauthorized"}), 401
         else:
             return jsonify({"message": "wrong token"}), 401
